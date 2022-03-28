7521e4c6b5825893ee5f6e8eb3674ae50d6f619d
hosenmdaltaf <altafhmeazee1999@outlook.com>
2022-03-28 18:12:17 +0600

    contact detail added

--- painting_main/homeapp/models.py
+++ painting_main/homeapp/models.py
@@ -38,6 +38,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=255, null=True)
     subject = models.CharField(max_length=255, null=True)
     message = models.TextField(max_length=255, null=True)
+    services = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return str(self.name) 
--- painting_main/homeapp/views.py
+++ painting_main/homeapp/views.py
@@ -1,13 +1,25 @@
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Services,Review
+from .models import Services,Review,Contact,Gallery
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
     services= Services.objects.all()
     reviews= Review.objects.all()
+    if request.method =="POST":
+        try:
+            name= request.POST.get('name')
+            email = request.POST.get('email')
+            phone= request.POST.get('phone')
+            services= request.POST.get('services')
+            message= request.POST.get('message')
+            Contact.objects.create(name=name,email=email,phone=phone,services=services,message=message)
+            return render(request,'global/thankyou.html')
+        except:
+            messages.warning(request, 'Please fill up all the form feild currectly!')
     context ={
         'services':services,
         'reviews':reviews
@@ -18,13 +30,45 @@ def about(request):
     return render(request,'homeapp/about.html')
 
 def works(request):
-    return render(request,'homeapp/works.html')
+    galleries = Gallery.objects.all()
+    return render(request,'homeapp/works.html',{'galleries':galleries})
 
 def services(request):
     return render(request,'homeapp/service.html')
 
 def contact(request):
+    if request.method =="POST":
+        try:
+            name= request.POST.get('name')
+            email = request.POST.get('email')
+            phone= request.POST.get('phone')
+            services= request.POST.get('services')
+            message= request.POST.get('message')
+            Contact.objects.create(name=name,email=email,phone=phone,services=services,message=message)
+            return render(request,'global/thankyou.html')
+        except:
+            messages.warning(request, 'Please fill up all the form feild currectly!')
+
     return render(request,'homeapp/contact.html')
+
+
+# def contact(request):
+#     if request.method == "POST":
+#         try:
+#             name = request.POST.get("name")
+#             email = request.POST.get("email")
+#             message = request.POST.get("message")
+#             phonenumber = request.POST.get("phonenumber") 
+#             print('----------------------test1------------------')
+#             print(name)
+#             print(email)
+#             print(message)
+#             print(phonenumber)
+#             Contact.objects.create(name=name,message=message,phonenumber=phonenumber,email=email)
+#             return render(request,'global/thankyou.html')
+#         except:
+#             messages.warning(request, 'Please fill up all the form feild currectly!')
+#     return render(request,'contactapp/contact.html')
 
 def services_details(request,pk):
     data = Services.objects.get(pk=pk)
