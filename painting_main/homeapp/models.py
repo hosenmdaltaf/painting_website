45f7f178c0d1f0649f92cf820ca5f82e1ac658b1
hosenmdaltaf <altafhmeazee1999@outlook.com>
2022-03-29 17:58:15 +0600

    complate for first host

--- painting_main/homeapp/models.py
+++ painting_main/homeapp/models.py
@@ -1,4 +1,5 @@
 
+from statistics import mode
 from django.db import models
 
 # from ckeditor_uploader.fields import RichTextUploadingField
@@ -10,7 +11,7 @@ class Services(models.Model):
     name= models.CharField(max_length=255, null=True)
     short_description =models.TextField(null=True,blank=True)
     details = models.TextField(null=True,blank=True)
-    thumnail_images =  models.ImageField(null=True,blank=True)
+    thumnail_images =  models.ImageField(null=True,blank=True,upload_to="services_images")
 
     def __str__(self):
         return str(self.name) 
@@ -21,7 +22,7 @@ class Services(models.Model):
 
 
 class Gallery(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to="gallery_images",null=True)
     name = models.CharField(max_length=255, null=True)
 
     def __str__(self):
@@ -49,3 +50,7 @@ class Review(models.Model):
 
     def __str__(self):
         return str(self.name) 
+
+class Slider(models.Model):
+    text= models.CharField(max_length=255, null=True)
+    slider_image= models.ImageField(upload_to="slider_images")
